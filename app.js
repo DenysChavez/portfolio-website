6ba22324b3fcced85c3f92c6eb5051a2292db4de
Denys <go4denys@gmail.com>
2023-01-19 21:18:59 -0500

    add home page, about page and portfolio page

---
+++ app.js
@@ -0,0 +1,39 @@
+const section = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+console.log(allSections);
+
+function pageTransition() {
+    //button clicked active class: add a class with the name "active-bn" and delete the name of "active-bn" class to the other classes
+    for (let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener("click", function() {
+            let currentBtn = document.querySelectorAll(".active-btn");
+            currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
+            this.className += " active-btn";
+        })
+    }
+
+    //Sections Active class: 
+    allSections.addEventListener("click", (e) => {
+        const id = e.target.dataset.id;
+        if (id) {
+            //remove selected from the other btns
+            sectBtn.forEach((btn) => {
+                btn.classList.remove("active")
+            })
+            e.target.classList.add("active");
+
+            //hide other sections
+            section.forEach((section) => {
+                section.classList.remove("active");
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add("active");
+        }
+    })
+}
+
+pageTransition();
